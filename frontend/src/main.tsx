baf9106a1cbe463673c05607afd88d384642d997
rzh4321 <rzh4321@gmail.com>
2024-10-10 22:04:53 -0400

    finished signup form

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,11 +8,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<LoginForm />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignupForm />} />
     </>,
   ),
 );
