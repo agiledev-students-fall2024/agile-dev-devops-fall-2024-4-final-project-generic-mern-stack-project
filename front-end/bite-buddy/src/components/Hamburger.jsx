f37296847f42376dc0b554c8424ce4a58cb6e410
ohiwop <pw2252@nyu.edu>
2024-10-18 17:46:48 -0400

    hamburger component

---
+++ front-end/bite-buddy/src/components/Hamburger.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './HamburgerDropdown.css';
+
+const Hamburger = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+    return (
+        <div className="hamburger-dropdown">
+            <button className="hamburger" onClick={toggleDropdown}>
+                &#9776;
+            </button>
+            {isOpen && (
+                <div className="dropdown-menu">
+                    <ul>
+                        <li><Link to="/challenges" onClick={() => setIsOpen(false)}>Challenges</Link></li>
+                        <li><Link to="/record" onClick={() => setIsOpen(false)}>Record Activity</Link></li>
+                        <li><Link to="/profile" onClick={() => setIsOpen(false)}>Profile</Link></li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Hamburger;
