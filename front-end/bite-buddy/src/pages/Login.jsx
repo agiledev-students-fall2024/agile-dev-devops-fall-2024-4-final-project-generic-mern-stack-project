59b30dd9a823bb9e55099a9d61a14621349c19c7
dsun03 <ds6760@nyu.edu>
2024-10-18 13:15:46 -0400

    #47 Create login page #49 Create signup page

---
+++ front-end/bite-buddy/src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import '../index.css'
+import { useNavigate } from 'react-router-dom';
+
+function Login(){
+
+    const navigate = useNavigate();
+    function goToSignUp(){
+        navigate('/signup')
+      }
+
+    return(<>
+        <h1 className='title'>Bite Buddy</h1>
+        <div className='registerDiv'>
+        <h2>Login</h2>
+        <form >
+            <label>Enter Username:<input type="text" placeholder="Username" /></label>
+            <label> Enter Password:<input type="password" placeholder="Password" /></label>
+            <button type="submit">Login</button>
+        </form>
+       
+        <a href='/signup'>Sign Up</a>
+        </div>
+        </>
+    );
+
+
+}
+export default Login
