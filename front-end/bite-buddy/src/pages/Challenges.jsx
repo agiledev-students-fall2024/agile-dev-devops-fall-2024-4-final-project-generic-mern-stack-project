96099b2e9e5ca007384b8546ff17a3658516dbfe
ohiwop <pw2252@nyu.edu>
2024-10-19 18:08:26 -0400

    added basic challenges page

--- front-end/bite-buddy/src/pages/Challenges.jsx
+++ front-end/bite-buddy/src/pages/Challenges.jsx
@@ -1,11 +1,34 @@
 import '../index.css'
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Challenges.css'
 
-function Challenges(){
 
-    return(
-        <p>Hellow World</p>
+//temp data placeholder
+const challengesData = [
+    { title: "Challenge Title", description: "text text text text text text text text text text text text text text text text ", image: "Challenge Image" },
+    { title: "Challenge Title", description: "text text text text text text text text text text text text text text text text ", image: "Challenge Image" },
+    { title: "Challenge Title", description: "text text text text text text text text text text text text text text text text ", image: "Challenge Image" },
+    { title: "Challenge Title", description: "text text text text text text text text text text text text text text text text ", image: "Challenge Image" },
+    { title: "Challenge Title", description: "text text text text text text text text text text text text text text text text ", image: "Challenge Image" }
+]
 
+const Challenges = () => {
+    return (
+        <div className="challenges-container">
+            <h1>CHALLENGES</h1>
+            {challengesData.map((challenge, index) => (
+                <div className="challenge-card" key={index}>
+                    <h2>CHALLENGE #{index + 1}</h2>
+                    <h3>{challenge.title}</h3>
+                    <p>{challenge.description}</p>
+                    <div className="challenge-image">{challenge.image}</div>
+                    {/* add component to button */}
+                    <button className="start-button">START CHALLENGE</button> 
+                </div>
+            ))}
+        </div>
     );
-}
+};
 
-export default Challenges
+export default Challenges;
