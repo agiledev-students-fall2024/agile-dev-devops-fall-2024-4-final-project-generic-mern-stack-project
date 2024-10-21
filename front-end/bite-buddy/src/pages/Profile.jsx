3257e925d3e462f6e960d9eee85014771c7e116e
ebc5802 <edison.chen@stern.nyu.edu>
2024-10-21 16:57:49 -0400

    created profile viewing page

--- front-end/bite-buddy/src/pages/Profile.jsx
+++ front-end/bite-buddy/src/pages/Profile.jsx
@@ -1,9 +1,31 @@
 import '../index.css'
+import { useNavigate } from 'react-router-dom';
 
 function Profile(){
 
+    const navigate = useNavigate();
+    function goToProfileSetup(){
+        navigate('/profileSetup')
+      }
+
     return(
-        <p>Hellow World</p>
+        <>
+            <h1 className='title'>Bite Buddy</h1>
+            <div className='registerDiv'>
+            <h2>Alexander Hamilton</h2>
+            <h3>Bio</h3>
+            
+            
+            <label>Email<input type="email" placeholder="alexander@gmail.com"  /></label>
+            <label>Last Name<input type="text" placeholder="Username"  /></label>
+            <label>Last Name<input type="text" placeholder="Username"  /></label>
+            <label>Enter Password: <input type="password" placeholder="Password"/></label>
+            <button type="submit">Edit Profile</button>
+            
+            <a className='alternative' href='/login'>Log in</a>
+        
+            </div>
+        </>
 
     );
 }
