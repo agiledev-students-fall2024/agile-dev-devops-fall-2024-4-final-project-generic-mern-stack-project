0104a4dc5e783452cdd160639610bec56872119a
nomegustaexam <rw2907@nyu.edu>
2024-10-22 17:46:41 -0400

    Implemented calendar for user story 6

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -8,6 +8,8 @@ import {
 import Homepage from './Homepage';
 import Tasks from './Tasks';
 import CreateTask from './CreateTask';
+import Calendar_monthly from './Calendar_monthly';
+
 function App() {
   return (
     <div className='App'>
@@ -17,6 +19,9 @@ function App() {
             <Route path='/' element={<Homepage/>} />
             <Route path="/Tasks" element={<Tasks />} />  {/* Task page route */}
             <Route path="/CreateTask" element={<CreateTask />} />
+            
+            {/* Rena's part : monthly Calendar */}
+            <Route path="/Monthly_calendar" element={<Calendar_monthly/>} />
           </Routes>
         </BrowserRouter>
       </div>
