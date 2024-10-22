d152bacdf5110698472f3de9f21c1dedeb749d64
mnsrt13 <75696910+mnsrt13@users.noreply.github.com>
2024-10-22 00:01:49 -0400

    Add hooks to profile
    * change profile picture file type

--- front-end/src/screens/Profile.js
+++ front-end/src/screens/Profile.js
@@ -10,32 +10,54 @@ import blockedData from '../fillerData/blocked.json'
 
 const Profile = () => {
     const { username } = useParams()
-    const user = userData.find(user => user.username === username)
-    
-    if (!user) ( <Navigate to='/' /> )
-
-    const getBlockedUsers = () => {
-        const blockedUsers = []
-
-        blockedData.forEach(item => {
-            if (item.blocked_id === loggedInData[0].id) {
-                blockedUsers.push(item.blocker_id)
-            } else if (item.blocker_id === loggedInData[0].id){
-                blockedUsers.push(item.blocked_id)
-            }
-        })
-
-        return blockedUsers
-    }
-
-    if (getBlockedUsers().includes(user.id))( <Navigate to='/' /> )
-
-    const belongsToLoggedIn = loggedInData[0].id === user.id 
-    const posts = postData.filter(post => post.author_id === user.id).sort((a, b) => new Date(b.date) - new Date(a.date))
+    const [user, setUser] = React.useState(null)
+    const [redirect, setRedirect] = React.useState(false)
+    const [belongsToLoggedIn, setBelongsToLoggedIn] = React.useState(null)
+    const [posts, setPosts] = React.useState([])
 
     const generateRandomList = (min, max, count) => (
         Array.from({ length: count }, () => Math.floor(Math.random() * (max - min + 1)) + min)
     )
+
+    React.useEffect(() => {
+        const fetchUser = () => {
+            const foundUser = userData.find(user => user.username === username)
+            if (!foundUser) {
+                setRedirect(true)
+            } else {
+                setUser(foundUser)
+                setBelongsToLoggedIn(loggedInData[0].id === foundUser.id)
+
+                const getBlockedUsers = () => {
+                    const blockedUsers = []
+            
+                    blockedData.forEach(item => {
+                        if (item.blocked_id === loggedInData[0].id) {
+                            blockedUsers.push(item.blocker_id)
+                        } else if (item.blocker_id === loggedInData[0].id){
+                            blockedUsers.push(item.blocked_id)
+                        }
+                    })
+
+                    return blockedUsers
+                }
+
+                const foundBlockedUsers = getBlockedUsers()
+                if (foundBlockedUsers.includes(foundUser.id)){
+                    setRedirect(true) 
+                }
+
+                setPosts(postData.filter(post => post.author_id === foundUser.id).sort((a, b) => new Date(b.date) - new Date(a.date)))
+            }
+        }
+
+        fetchUser()
+    }, [username])
+
+    
+    if (redirect) {
+        return <Navigate to='/' /> 
+    }
     
     const randomNumbers = generateRandomList(6, 13, posts.length)
 
@@ -148,37 +170,40 @@ const Profile = () => {
                         <path fillRule='evenodd' d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5'/>
                     </svg>
                 </Link>
-                {belongsToLoggedIn && <Link to='/' className='btn btn-secondary rounded-pill'>New Post</Link>}
+                { belongsToLoggedIn && <Link to='/createnewblogpost' className='btn btn-secondary rounded-pill'>New Post</Link> }
             </header>
-            <Container className='content' id='profile-blog-posts'>
-                <h1>{user.name}</h1>
-                <div className='default-img'>
-                    {
-                        user.profilePicture ? 
-                        <img src={user.profilePicture} alt={`${user.username}'s profile`} />:
-                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-person-circle' viewBox='0 0 16 16'>
-                            <path d='M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0'/>
-                            <path fillRule='evenodd' d='M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1'/>
-                        </svg>
+
+            { user && <>
+                <Container className='content' id='profile-blog-posts'>
+                    <h1>{user.name}</h1>
+                    <div className='default-img'>
+                        {
+                            user.profilePicture ? 
+                            <img src={user.profilePicture} alt={`${user.username}'s profile`} />:
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-person-circle' viewBox='0 0 16 16'>
+                                <path d='M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0'/>
+                                <path fillRule='evenodd' d='M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1'/>
+                            </svg>
+                        }
+                    </div>
+
+                    <p>{user.bio}</p>
+
+                    <div className='profile-functions mb-4'>
+                        {belongsToLoggedIn && <Link to='/edit-profile' className='btn btn-secondary rounded-pill'>Edit profile</Link>}
+                        {belongsToLoggedIn && <Link to='/friendslist' className='btn btn-secondary rounded-pill'>Friends</Link>}
+                    </div>
+                </Container>
+                <div className='mx-3 mb-5'>
+                    <hr />
+                    { 
+                        posts.length !== 0 ?
+                        renderPosts():
+                        <p className='text-center mt-5'>No posts yet</p>
+
                     }
                 </div>
-
-                <p>{user.bio}</p>
-
-                <div className='profile-functions mb-4'>
-                    {belongsToLoggedIn && <Link to='/edit-profile' className='btn btn-secondary rounded-pill'>Edit profile</Link>}
-                    {belongsToLoggedIn && <Link to='/friendslist' className='btn btn-secondary rounded-pill'>Friends</Link>}
-                </div>
-            </Container>
-            <div className='mx-3 mb-5'>
-                <hr />
-                { 
-                    posts.length !== 0 ?
-                    renderPosts():
-                    <p className='text-center mt-5'>No posts yet</p>
-
-                }
-            </div>
+            </> }
         </div>
     )
 }
