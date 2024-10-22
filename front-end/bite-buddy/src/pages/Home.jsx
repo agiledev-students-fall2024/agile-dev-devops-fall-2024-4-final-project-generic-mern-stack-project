3a44eee87c9580666ab2644dfc0ea90c08ab0b0b
dsun03 <ds6760@nyu.edu>
2024-10-22 00:13:26 -0400

    Add link to activity tracker and implement activity history

--- front-end/bite-buddy/src/pages/Home.jsx
+++ front-end/bite-buddy/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import '../index.css'
+import { useNavigate } from 'react-router-dom';
+
 
 function Home(){
+    const navigate = useNavigate();
+    function goToActivityTracker(){
+        navigate('/activity-tracker')
+    }
+    return(<>
+        <h1>HOME PAGE</h1>
+        <div className='weekly-acitivty-div'>
+            <button onClick={goToActivityTracker}>(click for) Weekly Activity</button>
 
-    return(
-        <p>Hello World</p>
-
+        </div>
+        </>
     );
 }
 
