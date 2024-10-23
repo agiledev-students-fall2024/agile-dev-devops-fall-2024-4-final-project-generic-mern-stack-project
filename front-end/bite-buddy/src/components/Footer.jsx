0f70645bd456b5369b11b5aa82d78b72b1f6acc4
ohiwop <pw2252@nyu.edu>
2024-10-23 18:07:06 -0400

    edited hamburger css, side menu now

--- front-end/bite-buddy/src/components/Footer.jsx
+++ front-end/bite-buddy/src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import '../index.css'
 import './HamburgerDropdown.css';
 import {Link} from 'react-router-dom';
-import {useState } from 'react';
+
+import React, { useState, useRef, useEffect } from 'react';
 import { IoHomeOutline } from "react-icons/io5";
 import { BsRecord2 } from "react-icons/bs";
 import { PiCookingPot } from "react-icons/pi";
 import { CgProfile } from "react-icons/cg";
 import { CiMedal } from "react-icons/ci";
+
 
 
 
@@ -89,30 +91,62 @@ function Footer({activeTab, setActiveTab}){
     );
 }
 
-function Hamburger({activeTab, setActiveTab}){
+function Hamburger({ activeTab, setActiveTab }) {
     const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setIsOpen((prev) => !prev);
     };
 
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className="hamburger-dropdown">
+        <div className="hamburger-dropdown" ref={dropdownRef}>
             <button className="hamburger" onClick={toggleDropdown}>
                 &#9776;
             </button>
             {isOpen && (
+                
                 <div className="dropdown-menu">
+
+                    <Link to="/home" 
+                    className="home-link" 
+                    onClick={() => { setActiveTab('home'); setIsOpen(false); }}>
+                    BITEBUDDY
+                    </Link>
+
+
                     <ul>
-                        <li><Link to="/challenges" onClick={() => {setActiveTab('challenges');setIsOpen(false)}}>Challenges</Link></li>
-                        <li><Link to="/record" onClick={() => {setActiveTab('record');setIsOpen(false)}}>Record Activity</Link></li>
-                        <li><Link to="/profile" onClick={() => {setActiveTab('profile');setIsOpen(false)}}>Profile</Link></li>
-                        <li><Link to="/login">Sign Out</Link></li>
+                        <li>
+                            <Link to="/challenges" onClick={() => { setActiveTab('challenges'); setIsOpen(false); }}><div className="navigation-icon"><CiMedal />Challenges</div></Link>
+                        </li>
+                        <li>
+                            <Link to="/record" onClick={() => { setActiveTab('record'); setIsOpen(false); }}><div className="navigation-icon"><BsRecord2 /> Record Activity</div></Link>
+                        </li>
+                        <li>
+                            <Link to="/profile" onClick={() => { setActiveTab('profile'); setIsOpen(false); }}><div className="navigation-icon"><CgProfile /> Profile</div></Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Sign Out</Link>
+                        </li>
                     </ul>
                 </div>
             )}
         </div>
     );
-};
+}
 
 export default Nav
