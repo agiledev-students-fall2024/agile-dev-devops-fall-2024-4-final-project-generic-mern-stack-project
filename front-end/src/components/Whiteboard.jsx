18f4565e9be796fd1d97cf9aac649e5f7519a42b
jackziyangchen <jackczy1209@gmail.com>
2024-10-24 13:12:49 -0400

    joined components together

--- front-end/src/components/Whiteboard.jsx
+++ front-end/src/components/Whiteboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Tldraw} from 'tldraw';
+import { Tldraw } from 'tldraw';
 import 'tldraw/tldraw.css';
 
 const Whiteboard = () => {
     return (
-        <div style={{ position: 'fixed', inset: 0}}>
-            <Tldraw />
+        <div className='w-full h-full'>
+            <Tldraw inferDarkMode />
         </div>
     )
 }
