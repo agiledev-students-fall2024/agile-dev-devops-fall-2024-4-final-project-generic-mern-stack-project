512c27f836b0d3cdbec97c87ad5576c1e4617900
wilsonxu101 <wilsonxu551@gmail.com>
2024-10-26 16:51:48 -0400

    Updated C19, need testing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,6 +12,8 @@ import InputField from "./components/InputField";
 import SubmitButton from "./components/SubmitButton";
 import LogoPageTitle from "./components/LogoPageTitle";
 import SearchBar from "./components/SearchBar";
+import PostBlogTextField from "./components/PostBlogTextField"; // C17
+import ChangeAccountInfoButton from "./components/ChangeAccountInfoButton" // C19
 import Home from "./pages/Home";
 import Community from "./pages/Community";
 import Blog from "./pages/Blog";
---
+++ front-end/src/components/ChangeAccountInfoButton.js
@@ -0,0 +1,14 @@
+//C19
+import React from 'react';
+import './ChangeAccountInfoButton.css';
+
+const ChangeAccountInfoButton = ({ label = "First Name", currentValue = "Mona" }) => {
+    return (
+        <div className="change-account-info-box">
+            <label className="info-label">{label}</label>
+            <p className="info-value">{currentValue}</p>
+        </div>
+    );
+};
+
+export default ChangeAccountInfoButton;
