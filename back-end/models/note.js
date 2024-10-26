e822196aad42a0c7342a810366be3c4cde277d8b
GavinZhengOI <gvzqaq@gmail.com>
2024-10-25 20:43:33 -0400

    add note.js

---
+++ back-end/models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  tags: [{
+    type: String
+  }],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
