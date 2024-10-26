c8eb4526fea9680b1195cf085a1f615ed448261b
wilsonxu101 <wilsonxu551@gmail.com>
2024-10-26 18:26:47 -0400

    added C14 Backbutton

---
+++ front-end/src/components/BackButton.js
@@ -0,0 +1,14 @@
+// C14
+import React from 'react';
+import './BackButton.css';
+import backButtonImage from '../assets/backbutton.png'; // Adjust the path based on your folder structure
+
+const BackButton = ({ onClick }) => {
+    return (
+        <button className="back-button" onClick={onClick}>
+            <img src={backButtonImage} alt="Back" className="back-button-image" />
+        </button>
+    );
+};
+
+export default BackButton;
