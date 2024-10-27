4535cfcc156010e19a4aaa79f0d4e3c599309874
radishsoups <theobsidianheart@gmail.com>
2024-10-27 18:18:55 -0400

    Adding CSS to Settings pages

--- front-end/src/pages/AccountSettings.js
+++ front-end/src/pages/AccountSettings.js
@@ -10,6 +10,7 @@ const AccountSettings = (props) => {
     const [data, setData] = useState([])
     const [popup, setPopup] = useState(false)
 
+    // keep track of state of popup
     const closePopup = () => {
         setPopup(false)
     }
@@ -30,6 +31,7 @@ const AccountSettings = (props) => {
             })
     }, [])
 
+    // if user did not click the link to the popup: render normally
     if (!popup) {
         return (
             <>
@@ -40,18 +42,20 @@ const AccountSettings = (props) => {
 
                 <h2>Account Information</h2>
 
-                <AccountInfo title={"Username"} text={data.username} />
-                <AccountInfo title={"Name"} text={data.name} />
-                <AccountInfo title={"Email"} text={data.email} />
-                <AccountInfo title={"Password"} text={data.password} />
-
+                <div className="account">
+                    <AccountInfo title={"Username"} text={data.username} />
+                    <AccountInfo title={"Name"} text={data.name} />
+                    <AccountInfo title={"Email"} text={data.email} />
+                    <AccountInfo title={"Password"} text={data.password} />
+                </div>
                 <h2>Deactivate Account</h2>
-                <p onClick={openPopup}>Delete your data and account</p>
+                <p className="delete-link" onClick={openPopup}>Delete your data and account</p>
 
                 <NavigationBar />
             </>
         )
     }
+    // open popup window for deactivation information
     else {
         return (
             <>
@@ -68,10 +72,11 @@ const AccountSettings = (props) => {
                 <AccountInfo title={"Password"} text={data.password} />
 
                 <h2>Deactivate Account</h2>
-                <p onClick={openPopup}>Delete your data and account</p>
+                <p className="delete-link" onClick={openPopup}>Delete your data and account</p>
                 <div className="Popup-box">
                     <div className="content">
-                        <p className="Popup-text">Deactivate</p>
+                        <p className="Popup-title">Are you sure you want to deactivate?</p>
+                        <p className="Popup-text">Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
                         <div className="buttons">
                             <Link to={'/signup'}><p className="Deactivate-yes">Deactivate my account</p></Link>
                             <p className="Close-button" onClick={closePopup}>Cancel</p>
