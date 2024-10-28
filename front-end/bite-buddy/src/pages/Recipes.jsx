60a61d9cdf098ac96b535691ab2fcf6fac225685
HarrisonDu <harrisondu01@gmail.com>
2024-10-28 11:50:26 -0400

    Recipe Page!

--- front-end/bite-buddy/src/pages/Recipes.jsx
+++ front-end/bite-buddy/src/pages/Recipes.jsx
@@ -6,9 +6,9 @@ function Recipes() {
   const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   const recipeData = [
-    { dish: 'Recipe 1',steps: ['Put your right foot in','Put your right foot out','Hokey Pokey'], imgs:['#','#','#'] },
-    { dish: 'Recipe 2' },
-    { dish: 'Recipe 3' },
+    { dish: 'Recipe 1',difficulty:"novice",description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',steps: ['Put your right foot in','Put your right foot out','Hokey Pokey'], imgs:['https://picsum.photos/400','https://picsum.photos/400','https://picsum.photos/400'] },
+    { dish: 'Recipe 2',difficulty:"novice",description:'lorem ipsum dish',steps: ['Put your right foot in','Put your right foot out','Hokey Pokey'], imgs:['https://picsum.photos/400','https://picsum.photos/400','https://picsum.photos/400']},
+    { dish: 'Recipe 3',difficulty:"novice",description:'lorem ipsum dish',steps: ['Put your right foot in','Put your right foot out','Hokey Pokey'], imgs:['https://picsum.photos/400','https://picsum.photos/400','https://picsum.photos/400']},
   ];
 
   const handleStartClick = (recipeItem) => {
@@ -20,32 +20,59 @@ function Recipes() {
   };
 
   return (
+    <html>
+    <body>
     <div>
       <h1 className="centered-header">RECIPES</h1>
-      <ul>
-        {recipeData.map((recipeItem, index) => (
-          <li key={index}>
-            <button onClick={() => handleStartClick(recipeItem)}>
-              {recipeItem.dish}
-            </button>
-          </li>
-        ))}
-      </ul>
+
+
+
+      
+      <div className = "recipeDashboardOuter">
+
+      <h2 className = "recipeHeader">Novice</h2>
+      {recipeData.map((recipeItem, index) => (
+        recipeItem.difficulty === "novice" && (
+        <div className = "recipeDashboardInner" rekey={index}>
+          <button className = "recipeCard" onClick={() => handleStartClick(recipeItem)}>
+            <div className = "recipeText">
+              <h1>{recipeItem.dish}</h1>
+              <p>{recipeItem.description}</p></div>
+ 
+            <br></br>
+            <img src = {recipeItem.imgs[0]} alt="oops,img problems!"></img>
+          </button>
+        </div>
+        )
+      ))}
+      </div>
+      
 
       {selectedRecipe && (
         <div className="popup-overlay" onClick={close}>
           <div className="popup-content" onClick={(e) => e.stopPropagation()}>
-            <h2>{selectedRecipe.dish}</h2>
-            <p>Steps for {selectedRecipe.dish}</p>
-            <p>{selectedRecipe.steps[0]}</p>
-            
+            <h1 >{selectedRecipe.dish}</h1>
+            <p><strong>Description:</strong> {selectedRecipe.description}</p>
 
+            <h2>Step 1: </h2>
+            <p>{selectedRecipe.steps[0]}</p>
+            <img src = {selectedRecipe.imgs[0]} alt="oops,img problems!"></img>
+
+            <h2>Step 2: </h2>
+            <p>{selectedRecipe.steps[1]}</p>
+            <img src = {selectedRecipe.imgs[1]} alt="oops,img problems!"></img>
+
+            <h2>Step 3: </h2>
+            <p>{selectedRecipe.steps[2]}</p>
+            <img src = {selectedRecipe.imgs[2]} alt="oops,img problems!"></img>
 
             <button className="close-button" onClick={close}>Close</button>
           </div>
         </div>
       )}
     </div>
+    </body>
+    </html>
   );
 }
 
