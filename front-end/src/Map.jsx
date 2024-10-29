819ca2f5fe590c73b20ce07e55a0fe27614a2868
afknero <en1135@nyu.edu>
2024-10-29 12:09:31 -0400

    Add Mapbox Geocoder and customize map controls

--- front-end/src/Map.jsx
+++ front-end/src/Map.jsx
@@ -1,9 +1,11 @@
 import { useState, useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
-import { SearchBox } from '@mapbox/search-js-react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { faTruckMedical } from '@fortawesome/free-solid-svg-icons';
 
 function Map() {
   const mapContainerRef = useRef();
@@ -21,17 +23,47 @@ function Map() {
       zoom: 13.5, // starting zoom
     });
 
-    // add navigation control
-    mapInstanceRef.current.addControl(new mapboxgl.NavigationControl());
+    // Add Mapbox Geocoder (Search)
+    mapInstanceRef.current.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        trackProximity: true,
+        // Customize marker options
+        marker: {
+          color: 'red',
+          draggable: true,
+        },
+        zoom: 14,
+        placeholder: 'Search for places',
+        // Customize flyTo animation options
+        flyTo: {
+          curve: 1,
+          duration: 1000,
+        },
+      }),
+      'top',
+    );
 
-    // add mapbox directions
+    // Add Navigation control to map
+    mapInstanceRef.current.addControl(
+      new mapboxgl.NavigationControl(),
+      'bottom-right',
+    );
+
+    // Add Mapbox Directions
     mapInstanceRef.current.addControl(
       new MapboxDirections({
         accessToken: mapboxgl.accessToken,
         unit: 'metric',
         profile: 'mapbox/cycling',
+        controls: {
+          inputs: true,
+          instructions: true,
+          profileSwitcher: false,
+        },
       }),
-      'top-left',
+      'top',
     );
 
     mapInstanceRef.current.on('load', () => {
@@ -41,16 +73,6 @@ function Map() {
 
   return (
     <>
-      <SearchBox
-        accessToken={process.env.REACT_APP_API_KEY}
-        map={mapInstanceRef.current}
-        mapboxgl={mapboxgl}
-        value={inputValue}
-        onChange={(d) => {
-          setInputValue(d);
-        }}
-        marker
-      />
       <div id="map-container" className="h-94 w-full" ref={mapContainerRef} />
     </>
   );
