4412cad8475a129b38d267dcd09b7e5b7b03a3aa
m4kenziejones <mrj8508@nyu.edu>
2024-10-29 19:32:05 -0400

    Commiting for Frontend Dboard without Submod

---
+++ front-end/easynote/src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProfileProvider } from './components/ProfileContext';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import NewNote from './components/NewNote';
+import ExistingNotes from './components/ExistingNotes';
+import MathNotes from './components/MathNotes';
+import Login from './components/Login';
+import Signup from './components/SignUp';
+import AuthGuard from './components/AuthGuard';
+
+const App = () => {
+  return (
+    <ProfileProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <AuthGuard>
+                  <Home />
+                </AuthGuard>
+              }
+            />
+            <Route
+              path="new-note"
+              element={
+                <AuthGuard>
+                  <NewNote />
+                </AuthGuard>
+              }
+            />
+            <Route
+              path="existing-notes"
+              element={
+                <AuthGuard>
+                  <ExistingNotes />
+                </AuthGuard>
+              }
+            />
+            <Route
+              path="math"
+              element={
+                <AuthGuard>
+                  <MathNotes />
+                </AuthGuard>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ProfileProvider>
+  );
+};
+
+export default App;
