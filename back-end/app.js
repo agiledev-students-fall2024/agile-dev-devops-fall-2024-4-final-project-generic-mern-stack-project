0ecda673aeb9e4c5729cda447684e6a1f609aa38
mnsrt13 <75696910+mnsrt13@users.noreply.github.com>
2024-11-01 11:23:54 -0400

    Setup Express server with routes and dependencies
    - Installed required dependencies in package.json (morgan, dotenv, cors, multer, axios, mocha, and chai)
    - Configured Express middleware (morgan, cors, body parsers) in app.js
    - Set up basic route structure with separate files for each route

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,26 @@
-// import and instantiate express
-const express = require("express") // CommonJS import style!
-const app = express() // instantiate an Express object
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
+const express = require("express") // import and instantiate express
+const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
+const cors = require('cors'); // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 
-// we will put some server logic here later...
+const app = express() // instantiate an Express object
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' })) // log all incoming requests, except when in unit test mode.  morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(cors()) // allow cross-origin resource sharing
+
+// use express's builtin body-parser middleware to parse any data included in a request
+app.use(express.json()) // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+// import route files
+const account = require('./routes/account');
+const friends = require('./routes/friends');
+const main = require('./routes/main');
+const posts = require('./routes/posts');
+
+//  use routes
+app.use('/api/friends', friends);
+app.use('/api/main', main);
+app.use('/api/posts', posts);
 
 // export the express app we created to make it available to other modules
 module.exports = app
