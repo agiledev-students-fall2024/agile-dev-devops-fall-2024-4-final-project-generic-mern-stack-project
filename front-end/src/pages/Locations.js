6743bb6787609d7a935f5a4ff43978dd63b84978
frederickrohn <fnr2009@nyu.edu>
2024-11-01 11:33:24 -0400

    built show members list functionality in locations page

--- front-end/src/pages/Locations.js
+++ front-end/src/pages/Locations.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import LocationCard from '../components/location/LocationCard';
+import TripMembersList from '../components/location/TripMembersList';
 import './Locations.css';
 import axios from 'axios';
 
@@ -8,6 +9,8 @@ const Locations = () => {
   const [locations, setLocations] = useState([]);
   const [tripStatus, setTripStatus] = useState(null); 
   const [loading, setLoading] = useState(true);
+  const [showMembers, setShowMembers] = useState(false); //this decides whether or not to show the members list
+  const [participants, setParticipants] = useState([]); // i think i need to use state here?
   const { tripId } = useParams();
 
   const fetchLocationsAndStatus = async () => {
@@ -28,9 +31,26 @@ const Locations = () => {
     }
   };
 
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        `https://mock-api-misty-fog-1131.fly.dev/api/trips/${tripId}`
+      );
+      const participants = response.data.participants;
+      setParticipants(participants);
+    } catch (error) {
+      console.error('Error fetching participants:', error);
+    }
+  };
+  
   useEffect(() => {
     fetchLocationsAndStatus();
   }, []);
+
+  const toggleMembersList = () => { //determines whether or not memberslist is shown
+    setShowMembers((prev) => !prev);
+    if (showMembers && participants.length === 0) {fetchUsers();}; //get the members if participants list not populated yet
+  };
 
   if (loading) return <p>Loading locations...</p>;
 
@@ -38,12 +58,16 @@ const Locations = () => {
     <div className="locations-page">
       <div className="locations-header">
         <h1>Locations</h1>
+        <button onClick={toggleMembersList} className="toggle-members-button">
+          {showMembers ? "Hide Members" : "Show Members"}
+        </button>
         {tripStatus !== 'completed' && (
           <Link to={`/add-location/${tripId}`} className="add-location-link">
             Add Location
           </Link>
         )}
       </div>
+      {showMembers && <TripMembersList participants={participants}/>}
       <div className="locations-grid">
         {locations.map((location) => (
           <LocationCard
