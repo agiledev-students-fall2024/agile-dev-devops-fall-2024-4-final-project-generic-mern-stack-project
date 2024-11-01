5941f5579b5f581fe2d28439e2396d3886163e50
avmvng <vb2310@nyu.edu>
2024-11-01 12:47:09 -0400

    added a dummy message to app.js to avoid errors

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 
-// we will put some server logic here later...
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+  })
 
 // export the express app we created to make it available to other modules
 module.exports = app
