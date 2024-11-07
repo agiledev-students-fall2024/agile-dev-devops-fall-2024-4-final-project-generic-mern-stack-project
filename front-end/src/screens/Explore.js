75f952ac52b41ed2979ebf5eb5a993791ca92db1
andy-612 <liubai.andy@gmail.com>
2024-11-07 14:30:18 -0500

    Removed console.log from previous commit

--- front-end/src/screens/Explore.js
+++ front-end/src/screens/Explore.js
@@ -14,7 +14,6 @@ const Explore = () => {
     const fetchData = async () => {
         try {
             const response = await axios.get(`${apiUrl}/api/explore`);
-            console.log(response.data)
             setUser(response.data.user)
             setPosts(response.data.posts)
         } catch (error) {
--- front-end/src/screens/Home.js
+++ front-end/src/screens/Home.js
@@ -14,7 +14,6 @@ const Home = () => {
     const fetchData = async () => {
         try {
             const response = await axios.get(`${apiUrl}/api/main`);
-            console.log(response.data)
             setUser(response.data.user)
             setPosts(response.data.posts)
         } catch (error) {
