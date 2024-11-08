dc41b3c8d69cf706d34c254e72b76ea0afbe13a0
avmvng <vb2310@nyu.edu>
2024-11-08 18:36:28 -0500

    stores search bar fetches sample stores from backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import filtersRoutes from "./routes/filtersRoutes.js";
+import storesRoutes from "./routes/storesRoutes.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -14,5 +15,7 @@ app.use(express.json());
 // routes
 app.use("/auth", authRoutes);
 app.use("/filters", filtersRoutes);
+app.use("/stores", storesRoutes);
+
 
 export default app;
