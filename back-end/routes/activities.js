e1b3c1a67b6ccb98e1555f844e4a98dee8f349b7
harrisonmangitwong <hmw5965@nyu.edu>
2024-11-09 15:50:01 -0500

    Added routes for first five "TO-DOs" for activities.js

--- back-end/routes/activities.js
+++ back-end/routes/activities.js
@@ -5,19 +5,61 @@ const router = express.Router();
 const activities = JSON.parse(fs.readFileSync('./mock-data/activities.json', 'utf-8'));
 
 // TODO: Get all activities (GET) - Retrieve and respond with a list of all activities in the system
-
+router.get('/', (req, res) => {
+    res.json(activities);
+  });  
 
 // TODO: Get a specific activity by ID (GET) - Retrieve details for the specified activity, including embedded comments
-
+router.get('/:activityId', (req, res) => {
+    const activityId = req.params.activityId;
+    const activity = activities.find(a => a.id === activityId);
+  
+    if (activity) {
+      res.json(activity);
+    } else {
+      res.status(404).json({ error: 'Activity not found' });
+    }
+  });
 
 // TODO: Create a new activity (POST) - Add a new activity within a location and respond with the newly created activity data
-
+router.post('/', (req, res) => {
+    const newActivity = {
+      id: `activity_${Date.now()}`,
+      ...req.body
+    };
+  
+    activities.push(newActivity);
+    fs.writeFileSync('./mock-data/activities.json', JSON.stringify(activities, null, 2), 'utf-8');
+    res.status(201).json(newActivity);
+  });  
 
 // TODO: Update activity information (PUT) - Modify the specified activity data and respond with the updated activity information
-
+router.put('/:activityId', (req, res) => {
+    const activityId = req.params.activityId;
+    const activityIndex = activities.findIndex(a => a.id === activityId);
+  
+    if (activityIndex !== -1) {
+      activities[activityIndex] = { ...activities[activityIndex], ...req.body };
+      fs.writeFileSync('./mock-data/activities.json', JSON.stringify(activities, null, 2), 'utf-8');
+      res.json(activities[activityIndex]);
+    } else {
+      res.status(404).json({ error: 'Activity not found' });
+    }
+  });  
 
 // TODO: Delete an activity (DELETE) - Remove the specified activity and respond with a confirmation message
-
+router.delete('/:activityId', (req, res) => {
+    const activityId = req.params.activityId;
+    const activityIndex = activities.findIndex(a => a.id === activityId);
+  
+    if (activityIndex !== -1) {
+      activities.splice(activityIndex, 1);
+      fs.writeFileSync('./mock-data/activities.json', JSON.stringify(activities, null, 2), 'utf-8');
+      res.json({ message: 'Activity deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Activity not found' });
+    }
+  });  
 
 // TODO: Upvote an activity (POST) - Increment the vote count for an activity and respond with the updated vote count
 
