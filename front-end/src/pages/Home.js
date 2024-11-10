b297fd37d356a11307b6e8dcbdda4e0c56929ce8
wilsonxu101 <wilsonxu551@gmail.com>
2024-11-10 17:18:12 -0500

    Update Home and Post page and env

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import BlogPost from '../components/BlogPost';
 import DropdownMenu from '../components/DropdownMenu';
@@ -6,23 +6,23 @@ import SearchBar from '../components/SearchBar';
 
 const Home = () => {
     const [searchInput, setSearchInput] = useState("");
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
 
     const handleSearch = () => {
         console.log(`Searching for: ${searchInput}`);
     };
 
-    useEffect(() => { 
-        axios("https://my.api.mockaroo.com/posts.json?key=3ac6ebb0")
-        .then(response => {
-            setPosts(response.data)
-        })
-        .catch(err => {
-            console.log(`No more requests allowed.`);
-            console.error(err);
-        });
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/home`)
+            .then(response => {
+                setPosts(response.data);
+            })
+            .catch(err => {
+                console.log("Failed to fetch posts.");
+                console.error(err);
+            });
     }, []);
-
 
     return (
         <div className="w-[100%] flex flex-col justify-center items-center gap-6 p-8 mx-auto md:w-[90%]">
@@ -41,14 +41,10 @@ const Home = () => {
             <div className="w-[100%] flex flex-col gap-4 md:w-[80%] lg:w-[70%]">
                 {posts.map(post => (
                     <div key={post.id}>
-                        <BlogPost
-                            post={post}
-                        />
-                    </div> 
+                        <BlogPost post={post} />
+                    </div>
                 ))}
             </div>
-
-            
         </div>
     );
 };
