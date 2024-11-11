fc8fecff8974c3e2c8e5ddab1f71a8629563ff22
avmvng <vb2310@nyu.edu>
2024-11-11 17:27:42 -0500

    Revert "Stores from database"

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,11 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
-import filtersRoutes from "./routes/filtersRoutes.js";
-import storesRoutes from "./routes/storesRoutes.js";
 import savedRoutes from "./routes/savedRoutes.js";
 import dotenv from "dotenv";
-import cors from "cors";
 
 dotenv.config();
+
+import cors from "cors";
 
 const app = express();
 
@@ -15,8 +14,6 @@ app.use(express.json());
 
 // routes
 app.use("/auth", authRoutes);
-app.use("/filters", filtersRoutes);
-app.use("/stores", storesRoutes);
 app.use("/route", savedRoutes)
 
 export default app;
