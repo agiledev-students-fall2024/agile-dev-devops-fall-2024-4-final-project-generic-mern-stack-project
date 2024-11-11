5a4289182b207c28afcdc4ede1028d0271df8e23
anthonyuri <aju2005@nyu.edu>
2024-11-11 14:58:10 -0500

    Updated Readme

--- front-end/src/pages/ProfilesPage.js
+++ front-end/src/pages/ProfilesPage.js
@@ -14,13 +14,12 @@ const ProfilesPage = () => {
   });
   const [isEditMode, setIsEditMode] = useState(false);
 
-  // Fetch user data from the API when the component mounts
   useEffect(() => {
     async function fetchUserData() {
       try {
         const response = await fetch('https://mock-api-misty-fog-1131.fly.dev/api/users/user_123');
         const data = await response.json();
-        const [firstName, lastName] = data.name.split(' '); // Split full name
+        const [firstName, lastName] = data.name.split(' '); 
         setUserData({ ...data, firstName, lastName });
       } catch (error) {
         console.error('Error fetching user data:', error);
@@ -29,12 +28,12 @@ const ProfilesPage = () => {
     fetchUserData();
   }, []);
 
-  // Toggle edit mode
+
   const handleEditToggle = () => {
     setIsEditMode((prevMode) => !prevMode);
   };
 
-  // Update user data in state as form values change
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setUserData((prevData) => ({
