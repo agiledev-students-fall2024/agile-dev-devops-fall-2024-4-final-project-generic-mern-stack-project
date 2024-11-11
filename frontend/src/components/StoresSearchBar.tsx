168f689c2b9eea1f6c276924e8bd97183fbf8ecc
avmvng <vb2310@nyu.edu>
2024-11-11 18:15:10 -0500

    Revert "changed port to fetch from to 3002"
    This reverts commit 47c1ef9b6ea1c84ffc83d3f62f359bbe70d17a9c.

--- frontend/src/components/StoresSearchBar.tsx
+++ frontend/src/components/StoresSearchBar.tsx
@@ -49,20 +49,15 @@ function StoreList({
   );
 }
 
-interface StoreSearchBarProps {
-
-  stores: Store[];
-}
-
-export default function StoreSearchBar({ stores: initialStores }: StoreSearchBarProps) {
+export default function StoreSearchBar() {
   const [open, setOpen] = useState(false);
-  const [stores, setStores] = useState<Store[]>(initialStores);
+  const [stores, setStores] = useState<Store[]>([]);
   const [suggestedStores, setSuggestedStores] = useState<Store[]>([]);
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {  // <-- useEffect to fetch stores from backend
-    fetch("http://localhost:3002/stores")
+    fetch("http://localhost:3001/stores")
       .then((response) => response.json())
       .then((data) => setStores(data))
       .catch((error) => console.error("Error fetching stores:", error));
