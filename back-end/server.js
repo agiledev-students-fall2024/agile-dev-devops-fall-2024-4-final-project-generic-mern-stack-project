ab86550cb536c9ecdec3a69de657ef72060beab7
nomegustaexam <rw2907@nyu.edu>
2024-11-12 00:52:11 -0500

    back-end for monthly calendar done

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 require('./config.js')
 const server = require("./app") // load up the web server
-const port = process.env.PORT // the port to listen to for incoming requests
+// the port to listen to for incoming requests
+const port = process.env.PORT || 4000; // Default to 4000 if PORT is not defined
 // call express's listen function to start listening to the port
 const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`)
