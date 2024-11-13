55076a9c2edc34326ba3f26fef28539785f67536
AndrewJ1234 <aj3556@nyu.edu>
2024-11-13 09:22:37 -0500

    updated app.js

--- back-end/app.js
+++ back-end/app.js
@@ -9,12 +9,12 @@ require("dotenv").config({ silent: true })
 
 const app = express();
 app.use(express.json()); 
+
+// for database part: sprint 3
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.log(err));
 app.use(cors())
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
 app.use('/api/notes', noteRoutes); 
 app.use('/api/users', userRoutes); 
 app.use('/api/auth/login', loginRoutes); 
