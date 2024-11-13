50a01d2e814f3b99a813d250aed7f327b219dc42
AndrewJ1234 <aj3556@nyu.edu>
2024-11-13 10:10:38 -0500

    added print statement to verify if token matches

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -4,6 +4,7 @@ const USER_SECRET = 'test_jwt_secret';
 
 function verifyToken(req, res, next) {
     const token = req.headers['authorization']?.split(' ')[1];
+    console.log('Hardcoded FakeToken', token); 
     if (!token) return res.status(403).json({ error: 'No token provided' });
 
     jwt.verify(token, USER_SECRET, (err, decoded) => {
