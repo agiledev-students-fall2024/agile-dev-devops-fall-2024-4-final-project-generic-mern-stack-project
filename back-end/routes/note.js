f9399d5d5a2452fbc6962dc07fdce0d0d6ec1d13
AndrewJ1234 <aj3556@nyu.edu>
2024-11-13 10:11:37 -0500

    updated axios post request to express server

--- back-end/routes/note.js
+++ back-end/routes/note.js
@@ -3,19 +3,18 @@ const Note = require('../models/note');
 const { verifyToken } = require('../middleware/auth'); //  token-verification
 const router = express.Router();
 
-
 router.post('/', verifyToken, async (req, res) => {
-    // console.log('Body', req.body); debugging
+    console.log('Body', req.body); 
 
-    const { title, content, tags, category, preview, updatedAt, user} = req.body;
-    console.log("Create new note post");
-    //const { title, content, tags } = req.body;
+    const { title, content, tags, category, preview, updatedAt, user, author} = req.body;
     console.log("Create new note post request");
+    // console.log('user id', req.userId) debugging for sprint 3
     const newNote = new Note({
         title,
         content,
         tags,
-        author: req.userId,
+        // author: req.userId,
+        author,
         category,
         preview,
         updatedAt,
@@ -26,9 +25,9 @@ router.post('/', verifyToken, async (req, res) => {
     //    await newNote.save(); // for the database part
     //    author: req.userId 
     //});
-
+    console.log(newNote)
     try {
-        await newNote.save();
+        // await newNote.save(); for sprint 3
         res.status(201).json(newNote);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -37,13 +36,14 @@ router.post('/', verifyToken, async (req, res) => {
 
 router.get('/', verifyToken, async (req, res) => {
     try {
-        console.log('testing');
-        const notes = await Note.find({ author: req.userId }).populate('author', 'username email');
-        res.status(200).json(notes);
+        console.log('test localhost:5000/api/notes');
+        // const notes = await Note.find({ author: req.userId }).populate('author', 'username email'); for sprint 3
+        // res.status(200).json(notes);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
+
 
 router.get('/:id', verifyToken, async (req, res) => {
     try {
