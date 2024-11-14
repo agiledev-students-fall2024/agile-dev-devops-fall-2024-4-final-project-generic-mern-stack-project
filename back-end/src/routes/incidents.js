71aa2d83039644b05e0526c4ee0bcc739980bf18
Ricky Ling <rl4093@nyu.edu>
2024-11-14 02:21:30 -0500

    add more endpoints

--- back-end/src/routes/incidents.js
+++ back-end/src/routes/incidents.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {
   getIncidents,
+  getIncidentById,
   reportIncident,
+  deleteIncident,
 } = require('../controllers/incidentController');
 
 /**
@@ -12,9 +14,21 @@ const {
 router.get('/', getIncidents);
 
 /**
+ * @route GET /api/incidents/:id
+ * @description Retrieve a single incident by ID.
+ */
+router.get('/:id', getIncidentById);
+
+/**
  * @route POST /api/incidents
  * @description Report a new incident.
  */
 router.post('/', reportIncident);
+
+/**
+ * @route DELETE /api/incidents/:id
+ * @description Delete an incident by ID.
+ */
+router.delete('/:id', deleteIncident);
 
 module.exports = router;
