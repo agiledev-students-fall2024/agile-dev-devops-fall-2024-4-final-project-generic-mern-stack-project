d97106549b8809d328b45906bff91c9e6ea65339
frederickrohn <fnr2009@nyu.edu>
2024-11-14 09:55:58 -0500

    wrote test for post activities route

--- back-end/test/activities.test.js
+++ back-end/test/activities.test.js
@@ -15,5 +15,26 @@ describe('activity routes', function() {
                 expect(activity.locationId).to.equal(locationId);
             });
         });
+
+        it('should return 404 if no activities are found', async function(){
+            const locationId = 'invalid location id';
+            const response = await request(app).get(`/activities/location/${locationId}`);
+
+            expect(response.status).to.equal(404);
+        });
+    });
+
+    describe('POST /activities', function() {
+        it('should create a new activity and return it with status 201', async function(){
+            const newAct = {
+                name: "nyu",
+                address: "181 Bleeker Street",
+                comments: []
+            };
+            const response = await request(app).post(`/activities`).send(newAct);
+
+            expect(response.status).to.equal(201);
+            expect(response.body).to.have.property('id').that.includes('activity_'); // check to see if other fields have been created
+        });
     });
 });
