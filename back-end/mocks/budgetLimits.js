4452a7e8ab423f42f8821e41d6e102b90987e505
chrisbobadilla <chrisbobadilla1@gmail.com>
2024-11-14 02:29:39 -0500

    updated budgetLimits to serve as a more realsiitc DB that stores a user's budget limits based on their user and budget id's

--- back-end/mocks/budgetLimits.js
+++ back-end/mocks/budgetLimits.js
@@ -1,14 +1,21 @@
-const budgetLimits = {
-    Bills: 1500,
-    Groceries: 300,
-    Health: 150,
-    Fuel: 100,
-    Shopping: 250,
-    Subscription: 100,
-    Transportation: 100,
-    Restaurant: 150,
-    MonthlyBudget: 3000,
-};
-
+const budgetLimits= [
+    {
+      budgetId: 1, // username's personal budget
+      userId: 1, // userId 1 is username's account
+      name: "Personal",
+      monthlyLimit: 3000,
+      categoryLimits: {
+        Bills: 1500,
+        Groceries: 300,
+        Health: 150,
+        Fuel: 100,
+        Shopping: 250,
+        Subscription: 100,
+        Transportation: 100,
+        Restaurant: 150,
+      },
+    },
+];
+  
 export default budgetLimits;
 
