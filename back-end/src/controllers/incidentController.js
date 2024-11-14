49f1e525a9e92bf0756feab0b045fbfd31e349b8
Ricky Ling <rl4093@nyu.edu>
2024-11-14 01:49:45 -0500

    add helper function

--- back-end/src/controllers/incidentController.js
+++ back-end/src/controllers/incidentController.js
@@ -1,7 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const incidentsPath = path.join(__dirname, '../data/incidents.json');
+
 /**
- * @module incidentController
+ * Reads incidents from the JSON file.
+ * @returns {Array} Array of incidents.
  */
-// const Incident = require('../models/Incident'); // Example model import
+const readIncidentsFromFile = () => {
+  const data = fs.readFileSync(incidentsPath, 'utf8');
+  return JSON.parse(data);
+};
+
+/**
+ * Writes incidents to the JSON file.
+ * @param {Array} incidents - Array of incidents to write to the file.
+ */
+const writeIncidentsToFile = (incidents) => {
+  fs.writeFileSync(incidentsPath, JSON.stringify(incidents, null, 2), 'utf8');
+};
 
 /**
  * Reports a new incident.
