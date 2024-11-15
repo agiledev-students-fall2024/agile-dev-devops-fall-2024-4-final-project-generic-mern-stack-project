0a763f246e2b136554f3d6cdfbb644f4dfb35cbc
mnsrt13 <75696910+mnsrt13@users.noreply.github.com>
2024-11-15 01:08:14 -0500

    Set up MongoDB

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
 const express = require("express") // import and instantiate express
+const mongoose = require('mongoose');
 const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
 const cors = require('cors'); // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 
@@ -10,6 +11,15 @@ app.use(cors()) // allow cross-origin resource sharing
 // use express's builtin body-parser middleware to parse any data included in a request
 app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+try {
+    mongoose.connect(process.env.MONGODB_URI)
+    console.log(`Connected to MongoDB.`)
+  } catch (err) {
+    console.log(
+      `Error connecting to MongoDB user account authentication will fail: ${err}`
+    )
+  }
 
 // import route files
 const account = require('./routes/account');
