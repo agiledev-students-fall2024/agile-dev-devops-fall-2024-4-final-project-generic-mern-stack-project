c262b75ce3384e14298bfd842e8257ccaf85c694
andy-612 <liubai.andy@gmail.com>
2024-11-21 10:14:11 -0500

    Updated schema for Blocked and Friend

--- back-end/models/Blocked.js
+++ back-end/models/Blocked.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 
-const blockedSchema = new mongoose.Schema({});
+const blockedSchema = new mongoose.Schema({
+  blocker_id: {
+    type: String, // Assuming IDs are strings (e.g., user IDs)
+    required: true,
+  },
+  blocked_id: {
+      type: String, // Assuming IDs are strings
+      required: true,
+  },
+  created_at: {
+      type: Date,
+      default: Date.now, // Automatically set the creation timestamp
+  },
+});
 
 const Blocked = mongoose.model('Blocked', blockedSchema);
 
