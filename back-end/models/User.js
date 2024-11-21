2f6e0f10c129b3c23c4d2ded948131f4c2cf8203
tobyywang <zw2669@nyu.edu>
2024-11-21 13:12:32 -0500

    Created db models

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  bio: { type: String, default: '' },
+  profilePicture: { type: String, default: '' },
+  layout: { type: String, enum: ['list', 'grid'], default: 'list' }
+});
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
