95f6d9a698e3c993c78a8b6b8e5d5e57173e3b41
frederickrohn <fnr2009@nyu.edu>
2024-11-24 18:05:16 -0500

    did the get location and get activities by location, frontend is connected

---
+++ back-end/controllers/locationsController.js
@@ -0,0 +1,25 @@
+import  Location from '../models/Location.js'; // Location model
+import  Activity from '../models/Activity.js'; // Activity model
+
+export const getLocation = async (req, res) => {
+    try{
+        const locationId = req.params.locationId;
+        const location = await Location.findById(locationId);
+        res.json(location);
+
+    }catch(error){
+        res.status(500).json({ error: 'Failed to retrieve activities for the location' });
+    };
+}
+
+export const getLocationActivities = async (req, res) => {
+    console.log('this is being used!')
+    try{
+        const locationId = req.params.locationId;
+        const location = await Location.findById(locationId).populate('activities');
+        res.json(location.activities);
+
+    }catch(error){
+        res.status(500).json({ error: 'Failed to retrieve activities for the location' });
+    };
+}
