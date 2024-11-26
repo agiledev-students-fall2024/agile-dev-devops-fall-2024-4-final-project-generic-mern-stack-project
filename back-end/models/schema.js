a8d548d45f63e21add312759a7b6c76ed8fa5608
nomegustaexam <99823268+nomegustaexam@users.noreply.github.com>
2024-11-26 12:35:18 -0500

    Revert "added login support"

--- back-end/models/schema.js
+++ back-end/models/schema.js
@@ -1,6 +1,5 @@
 require("../config")
 const mongoose = require("mongoose")
-const { name } = require("../app")
 const { Schema } = mongoose
 
 mongoose.connect(process.env.DSN) 
@@ -12,16 +11,8 @@ const TaskSchema = new mongoose.Schema({
     priority: {required: true, type: String},
     subject: {required: true, type: String},
     recurring: {required: true, type: String},
-    recurring_period: {type: String},
-    user_id: { type: Schema.Types.ObjectId, ref: "User" }
-  })
-
-  const UserSchema = new mongoose.Schema({
-    username: {required: true, type: String},
-    password_hash: {required: true, type: String},
-    name: {required: true, type: String}
+    recurring_period: {type: String}
   })
 
 const Task = mongoose.model("Task", TaskSchema)
-const User = mongoose.model("User", UserSchema)
 
