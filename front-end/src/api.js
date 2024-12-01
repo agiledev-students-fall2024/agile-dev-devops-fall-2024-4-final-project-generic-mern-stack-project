5d6c3e0cb3e050cc32fcbed865d110d876820437
bryantto08 <bryantto08@gmail.com>
2024-12-01 11:13:57 -0500

    created functions for whiteboard apis in frontend

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -39,3 +39,45 @@ export async function updateUserPassword(password) {
     const data = await response.json();
     return data;
 }
+
+export async function getWhiteboard(wbName) {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/whiteboard` + new URLSearchParams({
+        name: wbName
+    }), {
+        method: 'GET',
+
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export async function createWhiteboard(wbName) {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/whiteboard`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ wbName: wbName}),
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export async function deleteWhiteboard(wbName) {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/whiteboard`+ new URLSearchParams({
+        name: wbName
+    }), {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+    return data;
+}
--- front-end/src/pages/MeetingPage.jsx
+++ front-end/src/pages/MeetingPage.jsx
@@ -363,6 +363,7 @@ function MeetingPage() {
                         }
                         {
                             whiteboardVisible &&
+                            
                             <Whiteboard />
                         }
                     </div>
