1c8e9fad53b9942f2c0d30771bfc8d046fc64247
AndrewJ1234 <aj3556@nyu.edu>
2024-12-01 16:29:21 -0500

    updated jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,15 +6,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.css$": "identity-obj-proxy"
   },
-  roots: ["<rootDir>/test"],  // Make sure this is pointing to your test folder
+  roots: ["<rootDir>/test"],  
   transformIgnorePatterns: [
-    "/node_modules/(?!some-es6-module).+\\.js$" // Optionally, if there are any node_modules that need transforming
   ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!quill)/", // If you have third-party dependencies like quill that you want Jest to transform
-  ],
-
 };
