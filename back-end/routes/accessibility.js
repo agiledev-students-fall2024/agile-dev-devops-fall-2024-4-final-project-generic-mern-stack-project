72d03e714a8d5ae509e13ba016d0a73be4a6bf35
radishsoups <theobsidianheart@gmail.com>
2024-12-02 13:06:56 -0500

    Adding data validation to Accessibility

--- back-end/routes/accessibility.js
+++ back-end/routes/accessibility.js
@@ -3,6 +3,8 @@ import express from 'express'
 const router = express.Router();
 import Setting from "../models/setting.model.js";
 import { protectRouter } from "../middlewares/auth.middleware.js";
+import { check, validationResult } from 'express-validator';
+
 
 router.get("/api/color-mode", protectRouter, async (req, res) => {
     // get user's current color-mode
@@ -24,23 +26,37 @@ router.get("/api/color-mode", protectRouter, async (req, res) => {
     }
 });
 
-router.post("/api/color-mode", protectRouter, async (req, res) => {
-    // change user's current color-mode
-    try {
-        // getting user id from cookies
-        const id = req.user._id
+router.post("/api/color-mode", protectRouter,
+    [
+        check('color')
+            .isIn(['Light', 'Dark'])
+            .isString()
+            .withMessage("Request must be 'Light' or 'Dark'"),
+    ],
+    async (req, res) => {
+        // change user's current color-mode
+        const errors = validationResult(req);
 
-        const color = req.body.color
+        // validate request
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-        // update information
-        await Setting.updateOne({ userId: id }, { displayMode: color });
+        try {
+            // getting user id from cookies
+            const id = req.user._id
 
-        res.status(200).json(color)
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to change color mode' });
-    }
-});
+            const color = req.body.color
+
+            // update information
+            await Setting.updateOne({ userId: id }, { displayMode: color });
+
+            res.status(200).json(color)
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Failed to change color mode' });
+        }
+    });
 
 // router.get("/api/image-mode", protectRouter, async (req, res) => {
 //     // get user's current image mode
@@ -100,22 +116,35 @@ router.get("/api/font-size", protectRouter, async (req, res) => {
     }
 });
 
-router.post("/api/font-size", protectRouter, async (req, res) => {
-    // change user's current font size
-    try {
-        // getting user id from cookies
-        const id = req.user._id
+router.post("/api/font-size", protectRouter,
+    [
+        check('fontSize')
+            .isNumeric()
+            .withMessage("Request must be a valid number"),
+    ],
+    async (req, res) => {
+        // change user's current font size
+        const errors = validationResult(req);
 
-        const size = req.body.fontSize
+        // validate request
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-        // update information
-        await Setting.updateOne({ userId: id }, { fontSize: size });
+        try {
+            // getting user id from cookies
+            const id = req.user._id
 
-        res.status(200).json(size)
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to change font size' });
-    }
-});
+            const size = req.body.fontSize
+
+            // update information
+            await Setting.updateOne({ userId: id }, { fontSize: size });
+
+            res.status(200).json(size)
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Failed to change font size' });
+        }
+    });
 
 export default router
