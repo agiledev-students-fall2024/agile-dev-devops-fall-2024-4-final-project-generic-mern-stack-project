7ab0e5136703259eafd464b3e987b6aa3cbd9af1
jh7305 <jh7305@stern.nyu.edu>
2024-12-03 04:06:46 -0500

    server.js change

--- back-end/server.js
+++ back-end/server.js
@@ -19,14 +19,14 @@ const listener = app.listen(PORT, () => {
   console.log(`Server is running on port ${listener.address().port}`);
 });
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.error('Database connection error:', error));
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => console.error('Database connection error:', error));
 
 
 
