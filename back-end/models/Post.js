7667ac1a3f8866597a840aff45786d71a358d738
lsh7002 <lsh7002@nyu.edu>
2024-12-05 13:35:09 -0500

    cleaned up the comments

--- back-end/models/Post.js
+++ back-end/models/Post.js
@@ -1,57 +1,6 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  photo: {
-    type: String,
-    default: '', 
-  },
-
-  name: {
-    type: String,
-    default:'',
-  },
-});
-
-const Post = mongoose.model('Post', postSchema);
-
-module.exports = Post;
-
-
-
-/*
-// const mongoose = require('mongoose');
-
-// const postSchema = new mongoose.Schema({});
-
-// const Post = mongoose.model('Post', postSchema);
-
-// module.exports = Post;
-
-//schema exlains what the format of the data will be 
-
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-    // id:{
-    //     type: Number,
-    // },
     title: {
         type: String,
         required: true,
@@ -62,7 +11,6 @@ const postSchema = new mongoose.Schema({
     },
     author: {
         type: mongoose.Schema.Types.ObjectId,
-        // type: String,
         ref: 'User',
         required: true
     },
@@ -79,5 +27,3 @@ const postSchema = new mongoose.Schema({
 const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
-//take the schema and create a model from it
-*/
