91189c45311c9ae7ab99cd31c5084f68a5e40884
wilsonxu101 <wilsonxu551@gmail.com>
2024-12-07 13:17:55 -0500

    Delete old code for searchbar that was commented out

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -1,33 +1,3 @@
-/*
-import React from "react";
-
-import { BsSearchHeart } from "react-icons/bs";
-
-const SearchBar = ({ searchInput, setSearchInput, handleSearch }) => {
-
-  const handleChange = (e) => {
-    setSearchInput(e.target.value);
-    console.log(`Searching: ${e.target.value}`)
-  }
-
-  return (
-    <div className="flex flex-row justify-center w-[85%]">
-      <input
-        className="h-10 px-3 py-2 grow-0 w-[90%] bg-lavender_blush-900 text-ebony font-bold rounded-md placeholder-rose-600 rounded-r-none"
-        type="text"
-        placeholder="Search..."
-        onChange={handleChange}
-        value={searchInput || ''}
-      />
-      <button onClick={handleSearch} className="h-10 rounded-md bg-ebony-700 text-rose-700 hover:text-ebony-700 hover:bg-rose-700 font-bold px-2 w-[15%] md:w-[10%] lg:w-[8%] flex flex-col justify-center items-center rounded-l-none">
-        <BsSearchHeart size={24}/>
-      </button>
-
-    </div>
-  )
-}
-export default SearchBar;
-*/
 // should work for both seatrch bar in home and community
 import React from "react";
 import { BsSearchHeart } from "react-icons/bs";
