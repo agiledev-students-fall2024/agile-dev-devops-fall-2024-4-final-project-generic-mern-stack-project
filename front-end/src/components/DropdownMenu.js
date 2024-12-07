708c3bd3726e580f942eeebee7c52dfbaa76dd9e
wilsonxu101 <wilsonxu551@gmail.com>
2024-12-07 13:17:15 -0500

    delete old dropdonwmenu code

--- front-end/src/components/DropdownMenu.js
+++ front-end/src/components/DropdownMenu.js
@@ -1,24 +1,3 @@
-/*
-import React from "react"
-
-const DropdownMenu = (props) => {
-
-    return (
-        <div className="flex justify-center overflow-hidden px-4 py-2 bg-lavender_blush-900 border-[1px] border-rose rounded-md text-rose font-bold">
-            <select className="bg-transparent" value={props.label} name={props.name} onChange={props.onChange}>
-                <option>{props.label}</option>
-
-                {props.options.map(x => (
-                    <option key={props.options.indexOf(x)} className="px-4 py-2" value={x}>{x}</option>
-                ))}
-            </select>
-        </div>
-    )
-}
-
-export default DropdownMenu
-*/
-
 import React from "react";
 
 const DropdownMenu = ({ label, name, options, onChange, value }) => {
