1fa222c9cbdb71db231d86a457012425578f0d8f
wilsonxu101 <wilsonxu551@gmail.com>
2024-12-06 20:05:04 -0500

    Add titles

--- front-end/src/pages/Post.js
+++ front-end/src/pages/Post.js
@@ -61,6 +61,7 @@ const Post = () => {
 
   return (
     <div className="post-page-container">
+      <h1 className="text-xl font-bold text-ebony-700 text-center">Post</h1>
       {user && (
         <PostBlogTextField
           onCancel={handleCancel}
