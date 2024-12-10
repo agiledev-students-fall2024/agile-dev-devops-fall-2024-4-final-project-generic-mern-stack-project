9a128f08de8e3ff1f7369b05cf0d5debcc8e0579
lsh7002 <lsh7002@nyu.edu>
2024-12-10 13:13:00 -0500

    edit screen css

--- front-end/src/screens/Updateblogpost.js
+++ front-end/src/screens/Updateblogpost.js
@@ -96,6 +96,10 @@
 
 
 
+
+//correct
+
+
 import '../styles/Updateblogpost.css';
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
@@ -145,13 +149,14 @@ const UpdateBlogPost = () => {
 
   return (
     <div className="update-blog-post-container">
-      <header className="update-blog-post-header">
+    <header className="update-blog-post-header">
         <Link to={`/blogpostloggedin/${postId}`}>
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
           </svg>
         </Link>
-      </header>
+      </header> 
+
       <form className="update-blog-post-form" onSubmit={(e) => {
           e.preventDefault();
           handleUpdateBlog();
@@ -192,3 +197,8 @@ const UpdateBlogPost = () => {
 };
 
 export default UpdateBlogPost;
+
+
+
+
+
