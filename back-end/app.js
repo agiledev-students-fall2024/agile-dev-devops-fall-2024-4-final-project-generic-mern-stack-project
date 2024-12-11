4d4c4c2ba48ee863dc2e4832606ab82cf6dfab94
AndrewJ1234 <aj3556@nyu.edu>
2024-12-11 15:01:14 -0500

    dagasd

---
+++ back-end/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const noteRoutes = require('./routes/note'); 
+const userRoutes = require('./routes/user'); 
+const loginRoutes = require('./routes/login');
+const aiFeaturesRoute = require('./routes/aiFeaturesRoute');
+require("dotenv").config({ silent: true })
+
+const app = express();
+app.use(express.json()); 
+
+// for database part: sprint 3
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.log(err));
+app.use(cors())
+app.use('/api/notes', noteRoutes); 
+app.use('/api/users', userRoutes); 
+app.use('/api/auth/login', loginRoutes); 
+app.use('/api/aiFeaturesRoute', aiFeaturesRoute);
+
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ back-end/routes/aiFeaturesRoute.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const {transcribe} = require('./transcriberAI')
+const multer = require('multer');
+const uploadMulter = multer({ dest: 'uploads/' });
+const bodyParser = require('body-parser');
+const { summarizeText } = require('./aiFeatures');
+const noteRouter = require('./note');
+
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.use('/notes', noteRouter);
+
+router.post('/transcribe', uploadMulter.single('audio'), async (req, res) => {
+    try {
+        // console.log("Body", req.body); // for debugging
+        let audioSRC;
+        if (req.file) {
+            audioSRC = req.file.path;
+        }
+         else if(req.body.audio){
+             audioSRC = req.body.audio;
+        }
+        else{
+            return res.status(400).json({ error: 'No file uploaded or URL provided' });
+        }
+        const transcript = await transcribe(audioSRC);
+        const actualTranscript = transcript.text;
+        res.status(200).send({ actualTranscript });
+
+    } catch (error) {
+        console.error("Transcription Error", error);
+        res.status(500).json({ error: 'Failed to transcribe' });
+    }})
+
+
+router.post('/summarize', async (req, res) => {
+    const  text  = req.body.text;
+    console.log('Request Headers:', req.headers); // Log request headers
+    console.log('Request Body:', req.body);       // Log entire request body
+    if (!text) {
+        return res.status(400).json({ error: 'Text is required for summarization' });
+    }
+    try {
+        const summary = await summarizeText(text);
+        res.json({ summary });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to summarize text', message: error.message });
+    }
+});
+
+module.exports = router;
