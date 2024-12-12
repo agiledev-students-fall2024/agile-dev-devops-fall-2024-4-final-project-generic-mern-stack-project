c536da501b89be2eeb921b448ac1738250302e4c
Marcus Chau <mc8786@nyu.edu>
2024-12-11 21:53:45 -0500

    Added toggle for password show login

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode';
+import { Eye, EyeOff } from 'lucide-react';
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState(null);
     const navigate = useNavigate();
 
@@ -52,15 +54,22 @@ const LoginPage = () => {
                             className="w-full px-4 py-2 border rounded-lg"
                         />
                     </div>
-                    <div>
-                        <label className="block text-gray-700">Password</label>
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="w-full px-4 py-2 border rounded-lg"
-                        />
+                    <div className="relative">
+                            <label className="block text-gray-700">Password</label>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                className="w-full px-4 py-2 border rounded-lg"
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPassword(!showPassword)}
+                                className="absolute right-3 top-1/2 transform -translate-y-1 text-gray-500"
+                            >
+                                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
                     </div>
                     {error && <p className="text-red-500">{error}</p>}
                     <button className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">Login</button>
