7c2d2a3ed96a7db298697ca8fcb372aef6893d68
Rishi-Rana1 <rr4211@nyu.edu>
2024-12-11 22:59:04 -0500

    fix goal delete error

--- front-end/src/Goals/TaskAction.js
+++ front-end/src/Goals/TaskAction.js
@@ -6,7 +6,9 @@ const TaskModal = ({ goal, onClose, action, trigger, setTrigger }) => {
         try {
             const response = await fetch(`${process.env.REACT_APP_BACKEND}/delete/goals/${goal?._id}`, {
                 method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
+                headers: { 'Content-Type': 'application/json',
+                            'Authorization': window.localStorage.getItem('token')
+                 },
             });
 
             if (!response.ok) {
